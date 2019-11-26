ee754693c1ecf9a1779e65cc500a44755097d90f
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-11-26 23:28:25 +0300

    slider header

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,26 @@
 'use strict'
 
 $(document).ready(function() {
+
+  // Heder Slider
+  let $status = $('.header__pageInfo');
+  let $count = $('.header__count');
+  let $slickElement = $('.header__slider');
+
+  $slickElement.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+    let i = (currentSlide ? currentSlide : 0) + 1;
+    $status.text(i);
+    $count.text('/' + slick.slideCount);
+  });
   
-  $('.header__slider').slick({
-    infinite: true,
+  $slickElement.slick({
+    infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-    dots: true
+    dots: false,
+    prevArrow: '<div class="slick-arrow slick-arrow_prev"><i class="slick_arr slick_arr-left"></i></div>',
+    nextArrow: '<div class="slick-arrow slick-arrow_next"><i class="slick_arr slick_arr-right"></i></div>',
   });
 
 });
